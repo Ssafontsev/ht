bd11a280499d21e9a35fe6c7ffb646e96e58b3e6
Sergei Safontsev <ssafontsev@gmail.com>
2021-03-25 22:16:22 +0300

    new

--- Diplom/vk.py
+++ Diplom/vk.py
@@ -1,37 +1,28 @@
 import requests
 import time
+import json
 from tqdm import tqdm
 
-with open('Diplom/token.txt') as file_object:
-    token = file_object.read().strip()
-# with open('Diplom/yatoken.txt') as f:
-#     yatoken = f.read().strip()
-
-user_id = str(input('Введите user_id: '))
-
-
-yatoken = str(input('Введите ваш токен с полигона Яндекс: '))
 
 class VkUser:
     url = 'https://api.vk.com/method/'
 
-    def __init__(self, token1, version, user_id=None):
-        self.token = token1
+    def __init__(self, token, version):
+        self.token = token
         self.version = version
-        self.user_id = user_id
         self.params = {
             'access_token': self.token,
             'v': self.version,
         }
         self.owner_id = requests.get(self.url + 'users.get', self.params).json()['response'][0]['id']
 
-    def get_photos_links(self, user_id: str):
+    def get_photos_links(self, user_id: str, qty_photos: str):
         if user_id is None:
             user_id = self.owner_id
         followers_url = self.url + 'photos.get'
         followers_params = {
             'album_id': 'profile',
-            'count': 5,
+            'count': qty_photos,
             'user_id': user_id,
             'extended': 1
         }
@@ -52,41 +43,50 @@ class VkUser:
             photo_info['file_name'] = photo_name
             photo_info['size'] = biggest_photo['type']
             photos_to_upload.append(photo_info)
+        with open('Diplom/about_photos.json', 'w') as json_file:
+            json.dump(photos_to_upload, json_file)
         return url_names
 
 
 class YaUploader:
-    def __init__(self, yatoken: str):
-        self.yatoken = yatoken
+    def __init__(self, token: str, user_id: str):
+        self.token = token
+        self.user_id = user_id
 
-    def create_dir(self, user_id):
-        """Создает папку на яндекс диске"""
+    def create_dir(self):
         url = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {'path': user_id}
-        headers = {'Authorization': self.yatoken}
+        params = {'path': self.user_id}
+        headers = {'Authorization': self.token}
         response = requests.put(url, params=params, headers=headers)
-        # проверка выполнения создания папки
         if response.status_code == 201:
-            print(f'Папка "{user_id}" создана на диске.')
+            print(f'Папка "{self.user_id}" создана на диске.')
         else:
             error_message = response.json()['message']
             print(f'Ошибка код - {response.status_code}: {error_message}')
 
-    def upload(self, file_path: str):
-        """Метод загружает файл на яндекс диск"""
-        for link, name in vk_client.get_photos_links(user_id):
+    def upload(self, lst_link_name):
+        for link, name in tqdm(lst_link_name):
             response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
-                                     params={'path': f"{user_id}/{name}",
+                                     params={'path': f"{self.user_id}/{name}",
                                              'url': link},
-                                     headers={'Authorization': f'OAuth {self.yatoken}'})
-            for i in tqdm(vk_client.get_photos_links(user_id)):
-                if response.status_code == 202:
-                    time.sleep(1)
+                                     headers={'Authorization': f'OAuth {self.token}'})
+            if response.status_code == 202:
+                time.sleep(0.1)
         return
 
 
-vk_client = VkUser(token, '5.130')
-
-uploader = YaUploader(yatoken)
-uploader.create_dir(user_id)
-uploader.upload(vk_client.get_photos_links(user_id))
+if __name__ == '__main__':
+    with open('Diplom/token.txt') as file_object:
+        tokenvk = file_object.read().strip()
+    with open('Diplom/yatoken.txt') as f:
+        yatoken = f.read().strip()
+    user_id_input = str(input('Введите user_id: '))
+    qty_photos_input = str(input('Введите количество фото для загрузки: '))
+    # yatoken = str(input('Введите ваш токен с полигона Яндекс: '))
+    # tokenvk = str(input('Введите ваш токен VK: '))
+    vk_client = VkUser(tokenvk, '5.130')
+    # vk_client.get_photos_links(user_id_input, qty_photos_input)
+    listoflinks = vk_client.get_photos_links(user_id_input, qty_photos_input)
+    uploader = YaUploader(yatoken, user_id_input)
+    uploader.create_dir()
+    uploader.upload(listoflinks)
