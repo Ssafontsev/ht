a4eea2ca14cf322a8d0d5841e8633a9dd9bf770c
Sergei Safontsev <ssafontsev@gmail.com>
2021-09-25 10:29:06 +0300

    new

--- scratch_4.py
+++ scratch_4.py
@@ -1,14 +1,27 @@
 import sqlalchemy
 from pprint import pprint
 
-engine = sqlalchemy.create_engine('postgresql://postgres:admin@localhost:5432/postgres')
+engine = sqlalchemy.create_engine('postgresql://postgres:admin@localhost:5432/hometask')
 engine
 connection = engine.connect()
 
-# sel = connection.execute("""SELECT * FROM film;
-# """).fetchone()
-# sel = connection.execute("""SELECT title FROM film;
-# """).fetchmany(10)
-sel = connection.execute("""SELECT first_name, last_name FROM actor;
-""").fetchmany(10)
+# sel = connection.execute("""SELECT album_name, year FROM album
+# WHERE year = 2018;
+# """).fetchall()
+# sel = connection.execute("""SELECT title, time FROM track
+# WHERE time = (SELECT MAX(time) FROM track);
+# """).fetchall()
+# sel = connection.execute("""SELECT title, time FROM track
+# WHERE time >= 3.5;
+# """).fetchall()
+# sel = connection.execute("""SELECT title FROM collection
+# WHERE year >= 2018 and year <= 2020;
+# """).fetchall()
+# sel = connection.execute("""SELECT name FROM artist
+# WHERE name NOT LIKE ('%% %%');
+# """).fetchall()
+# sel = connection.execute("""SELECT title FROM track
+# WHERE title iLIKE ('%%my%%');
+# """).fetchall()
+
 pprint(sel)
